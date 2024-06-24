74c3c686100b428f8a844a8b2f76935a73dc6ee5
Thouma <richard_allcca_llano@hotmail.com>
2024-06-23 19:46:08 -0500

    backup 23-06-24

--- src/Components/shopping/ShoppingCart.js
+++ src/Components/shopping/ShoppingCart.js
@@ -2,60 +2,60 @@ import { useReducer } from "react";
 // reducer
 import { TYPES } from "../../actions/shoppingAction";
 import {
-   shoppingInitialState,
-   shoppingReducer,
+  shoppingInitialState,
+  shoppingReducer,
 } from "../../reducers/shoppingReducer";
 // components
 import CartItem from "./CartItem";
 import ProductItem from "./ProductItem";
 import Total from "./Total";
 
+
 const ShoppingCart = () => {
-   const [ state, dispatch ] = useReducer(shoppingReducer, shoppingInitialState);
+  const [ state, dispatch ] = useReducer(shoppingReducer, shoppingInitialState);
 
-   const { products, cart } = state;
+  const { products, cart } = state;
 
-   const addToCart = (id) => {
-      dispatch({ type: TYPES.ADD_TO_CART, payload: id });
-   };
+  const addToCart = (id) => {
+    dispatch({ type: TYPES.ADD_TO_CART, payload: id });
+  };
 
-   const delFromCart = (id, all = false) => {
-      // valida si elimina todos de un tipo o solo uno
-      if (all) {
-         dispatch({ type: TYPES.REMOVE_ALL_TO_CART, payload: id });
-      } else {
-         dispatch({ type: TYPES.REMOVE_ONE_TO_CART, payload: id });
-      }
-   };
+  const delFromCart = (id, all = false) => {
+    if (all) {// valida si elimina todos
+      dispatch({ type: TYPES.REMOVE_ALL_TO_CART, payload: id });
+    } else { // o un solo tipo
+      dispatch({ type: TYPES.REMOVE_ONE_TO_CART, payload: id });
+    }
+  };
 
-   const clearCart = () => dispatch({ type: TYPES.CLEAR_CART });
+  const clearCart = () => dispatch({ type: TYPES.CLEAR_CART });
 
-   return (
-      <div>
-         <h2>Shopping Cart</h2>
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
 
-         <h3>Productos</h3>
-         <article className="box grid-responsive">
-            {products.map((product) => (
-               <ProductItem
-                  key={product.id}
-                  data={product}
-                  addToCart={addToCart}
-               />
-            ))}
-         </article>
+      <h3>Productos</h3>
+      <article className="box grid-responsive">
+        {
+          products.map((product) => (
+            <ProductItem key={ product.id } data={ product } addToCart={ addToCart } />
+          ))
+        }
+      </article>
 
-         <h3>Carrito</h3>
-         <article className="box">
-            <button onClick={clearCart}>Limpiar Carrito</button>
-            {cart.map((item, index) => (
-               <CartItem key={index} data={item} delFromCart={delFromCart} />
-            ))}
-         </article>
+      <h3>Carrito</h3>
+      <article className="box">
+        <button onClick={ clearCart }>Limpiar Carrito</button>
+        {
+          cart.map((item, index) => (
+            <CartItem key={ index } data={ item } delFromCart={ delFromCart } />
+          ))
+        }
+      </article>
 
-         <Total data={cart} />
-      </div>
-   );
+      <Total data={ cart } />
+    </div>
+  );
 };
 
 export default ShoppingCart;
--- src/reducers/shoppingReducer.js
+++ src/reducers/shoppingReducer.js
@@ -1,65 +1,66 @@
 import { TYPES } from "../actions/shoppingAction";
 
+
 export const shoppingInitialState = {
-   products: [
-      { id: 1, name: "Product 1", price: 100 },
-      { id: 2, name: "Product 2", price: 200 },
-      { id: 3, name: "Product 3", price: 300 },
-      { id: 4, name: "Product 4", price: 400 },
-      { id: 5, name: "Product 5", price: 500 },
-      { id: 6, name: "Product 6", price: 600 },
-   ],
-   cart: [],
+  products: [
+    { id: 1, name: "Product 1", price: 100 },
+    { id: 2, name: "Product 2", price: 200 },
+    { id: 3, name: "Product 3", price: 300 },
+    { id: 4, name: "Product 4", price: 400 },
+    { id: 5, name: "Product 5", price: 500 },
+    { id: 6, name: "Product 6", price: 600 },
+  ],
+  cart: [],
 };
 
 export function shoppingReducer(state, action) {
-   switch (action.type) {
-      case TYPES.ADD_TO_CART: {
-         let newItem = state.products.find((p) => p.id === action.payload);
-         let itemInCart = state.cart.find((item) => item.id === newItem.id);
-         return itemInCart
-            ? {
-               ...state, //si existe agrega 1 a quantity
-               cart: state.cart.map((item) =>
-                  item.id === newItem.id
-                     ? { ...item, quantity: item.quantity + 1 }
-                     : item
-               ),
-            }
-            : {
-               ...state, // si no existe crea quantity = 1
-               cart: [ ...state.cart, { ...newItem, quantity: 1 } ],
-            };
-      }
-      case TYPES.REMOVE_ONE_TO_CART: {
-         let itemToDelete = state.cart.find((item) => item.id === action.payload);
-         return itemToDelete.quantity > 1
-            ? {
-               ...state,
-               cart: state.cart.map((item) =>
-                  item.id === action.payload
-                     ? { ...item, quantity: item.quantity - 1 }
-                     : item
-               ),
-            }
-            : {
-               ...state,
-               cart: state.cart.filter((item) => item.id !== action.payload),
-            };
-      }
-      case TYPES.REMOVE_ALL_TO_CART: {
-         return {
-            ...state,
-            cart: state.cart.filter((item) => item.id !== action.payload),
-         };
-      }
-      case TYPES.CLEAR_CART: {
-         return shoppingInitialState;
-      }
+  switch (action.type) {
+    case TYPES.ADD_TO_CART: {
+      let newItem = state.products.find((p) => p.id === action.payload);
+      let itemInCart = state.cart.find((item) => item.id === newItem.id);
+      return itemInCart
+        ? {
+          ...state, //si existe agrega 1 a quantity
+          cart: state.cart.map((item) =>
+            item.id === newItem.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        }
+        : {
+          ...state, // si no existe crea quantity = 1
+          cart: [...state.cart, { ...newItem, quantity: 1 }],
+        };
+    }
+    case TYPES.REMOVE_ONE_TO_CART: {
+      let itemToDelete = state.cart.find((item) => item.id === action.payload);
+      return itemToDelete.quantity > 1
+        ? {
+          ...state,
+          cart: state.cart.map((item) =>
+            item.id === action.payload
+              ? { ...item, quantity: item.quantity - 1 }
+              : item
+          ),
+        }
+        : {
+          ...state,
+          cart: state.cart.filter((item) => item.id !== action.payload),
+        };
+    }
+    case TYPES.REMOVE_ALL_TO_CART: {
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+    }
+    case TYPES.CLEAR_CART: {
+      return shoppingInitialState;
+    }
 
-      default:
-         return state;
-   }
+    default:
+      return state;
+  }
 }
 // Notas:
 // ...state, => todo lo que viene de state
